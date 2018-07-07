93935f8d7f2ed15ad75843454b2ab90e59f30f25
Ajyoon1 <40322864+Ajyoon1@users.noreply.github.com>
2018-07-07 18:24:27 -0400

    Updated with working version

--- complete.R
+++ complete.R
@@ -1,5 +1,5 @@
 # Complete cases function
-# It counts 
+# It counts occurrence of complete cases by ID and returns the data frame 
 complete <- function(directory, id = 1:332) {
   all_files <- list.files(directory)
   files_scope <- all_files[id]
@@ -7,14 +7,9 @@ complete <- function(directory, id = 1:332) {
   for (f in files_scope[-1]) {
     DF <- rbind(DF, read.csv(file.path(directory, f)), header = TRUE)
   }
-#  complete_DF <- c()  ## initialize complete_DF
-  df <- data.frame(c("", ""))
-  colnames(df) <- c("ID", "nobs")
-  for (f in id)
-    df <- rbind(df, (c(id, length(DF[complete.cases(DF$sulfate, DF$nitrate)&&DF$ID==id]))))
-#   c(id, legnth(DF[complete.cases(DF$sulfate, DF$nitrate)&&(DF$ID==id)]
-#   complete_DF <- c(complete_DF, complete.cases(DF$sulfate, DF$nitrate) 
-#   complete_DF <- complete.cases(DF$sulfate, DF$nitrate)
-#  return(length(complete_DF[complete_DF==TRUE]))
-  return(df)
+## Select all rows with complete observations for sulfate and nitrate columns
+  df <- DF[complete.cases(DF[ , 2:3]), ]
+  df_count <- data.frame(table(df$ID))
+  colnames(df_count) <- c("id", "nobs")
+  return(df_count)
   }
